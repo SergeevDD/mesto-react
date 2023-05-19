81d85a82c59391f2eb95a3ae157f879ef2735bb8
Sergeev Dmitry <sy1er@yandex.ru>
2023-05-19 17:44:46 +0400

    refactor: переделать функционал валидации, и кнопку загрузки

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ import Footer from './Footer.js'
 import AddPlacePopup from './AddPlacePopup.js';
 import EditProfilePopup from './EditProfilePopup.js';
 import EditAvatarPopup from './EditAvatarPopup.js';
+import DeleteCardPopup from './DeleteCardPopup';
 import ImagePopup from './ImagePopup.js';
 import { api } from '../utils/Api.js'
 import { CurrentUserContext } from '../contexts/CurrentUserContext.js'
@@ -31,53 +32,74 @@ function App() {
     api.toggleLike(isLiked, card._id)
       .then((newCard) => {
         setCards((state) => state.map((c) => c._id === card._id ? newCard : c));
-      });
+      })
+      .catch((err) => console.log('Ошибка:', err))
+  }
+  function confirmDelete() {
+    setIsLoading(true)
+    api.removeCard(isConfirmed)
+      .then(() => {
+        setCards(state => state.filter((c) => c._id !== isConfirmed));
+      })
+      .catch((err) => console.log('Ошибка:', err))
+      .finally(() => {
+        setIsLoading(false)
+        setDeletePopup(false);
+      })
   }
 
   function handleCardDelete(id) {
-    console.log(id);
-    api.removeCard(id)
-      .then(() => {
-        setCards(state => state.filter((c) => c._id !== id));
-      });
+    setDeletePopup(true)
+    setIsConfirmed(id)
   }
 
   function handleUpdateUser(user) {
+    setIsLoading(true)
     api.setUserData(user)
       .then((newUser) => {
         setCurrentUser(newUser);
         setProfilePopup(false);
-      });
+      })
+      .catch((err) => console.log('Ошибка:', err))
+      .finally(() => setIsLoading(false))
   }
 
   function handleUpdateAvatar(link) {
+    setIsLoading(true)
     api.setUserAvatar(link)
       .then((newUser) => {
         setCurrentUser(newUser);
         setAvatarPopup(false);
-      });
+      })
+      .catch((err) => console.log('Ошибка:', err))
+      .finally(() => setIsLoading(false))
   }
 
   function handleAddPlaceSubmit(card) {
+    setIsLoading(true)
     api.uploadCard(card)
       .then((newCard) => {
-        setCards([newCard,...cards]);
+        setCards([newCard, ...cards]);
         setAddPopup(false);
-      });
+      })
+      .catch((err) => console.log('Ошибка:', err))
+      .finally(() => setIsLoading(false))
   }
 
   function closeAllPopups() {
     setAvatarPopup(false);
     setProfilePopup(false);
     setAddPopup(false);
+    setDeletePopup(false);
     setSelectedCard({});
   }
 
-
-
+  const [isConfirmed, setIsConfirmed] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
   const [isEditProfilePopupOpen, setProfilePopup] = useState(false);
   const [isAddPlacePopupOpen, setAddPopup] = useState(false);
   const [isEditAvatarPopupOpen, setAvatarPopup] = useState(false);
+  const [isDeletePopupOpen, setDeletePopup] = useState(false);
   const [selectedCard, setSelectedCard] = useState({})
   const [currentUser, setCurrentUser] = useState(
     {
@@ -114,25 +136,34 @@ function App() {
       <Footer />
       <AddPlacePopup
         isOpen={isAddPlacePopupOpen}
+        isLoading={isLoading}
         onClose={closeAllPopups}
         onAddPlace={handleAddPlaceSubmit}
       />
 
       <EditProfilePopup
         isOpen={isEditProfilePopupOpen}
+        isLoading={isLoading}
         onClose={closeAllPopups}
         onUpdateUser={handleUpdateUser}
       />
 
       <EditAvatarPopup
         isOpen={isEditAvatarPopupOpen}
+        isLoading={isLoading}
         onClose={closeAllPopups}
         onUpdateAvatar={handleUpdateAvatar}
       />
 
+      <DeleteCardPopup
+        isOpen={isDeletePopupOpen}
+        isLoading={isLoading}
+        onClose={closeAllPopups}
+        onDeleteCard={confirmDelete}
+      />
+
       <ImagePopup
         card={selectedCard}
-
         onClose={closeAllPopups}
       />
     </CurrentUserContext.Provider>
